50b9526276a5946d85686a1fce0c2daa2e31df39
endlessspring.of <endlesssspring.of@gmail.com>
2021-03-07 22:06:16 +0300

    v1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const {
   PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD
+  PHASE_PRODUCTION_BUILD,
 } = require('next/constants')
 
 // This uses phases as outlined here: https://nextjs.org/docs/#custom-configuration
@@ -10,19 +10,19 @@ module.exports = (phase) => {
   // when `next build` or `npm run build` is used
   const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
   // when `next build` or `npm run build` is used
-  const isStaging = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
+  const isStaging =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
 
   const env = {
     MAIN_DOMAIN: (() => {
       if (isStaging) return ''
       if (isDev) return ''
       if (isProd) return ''
-    })()
-
+    })(),
   }
 
   // next.config.js object
   return {
-    env
+    env,
   }
 }
--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -4,11 +4,15 @@ import { Home } from '../../pages/index'
 
 describe('Home page', () => {
   it('matches snapshot', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     const { asFragment } = render(<Home />, {})
     expect(asFragment()).toMatchSnapshot()
   })
 
   it('clicking button triggers alert', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     const { getByText } = render(<Home />, {})
     window.alert = jest.fn()
     fireEvent.click(getByText('Test Button'))
